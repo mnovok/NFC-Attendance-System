f1904335360e981bf73fe6052c8143adf5a59339
ninarada <nradja00@fesb.hr>
2025-01-22 12:30:17 +0100

    fixed saving attendance

--- backend/controllers/nfcController.js
+++ backend/controllers/nfcController.js
@@ -5,29 +5,31 @@ const Class = require('../models/Class');
 // @desc    Saves attendance
 // @route   POST /api/nfc
 const saveAttendance =  async (req, res) => {
-    const { uid } = req.body; // Extract UID from the request body
+    const { uid } = req.body;
 
     if (!uid) {
         return res.status(400).send({ message: 'UID is missing' });
     }
 
     try {
-        // Find the student by UID
         const student = await User.findOne({ uid: uid });
 
         if (!student) {
-        return res.status(404).send({ message: 'Student not found' });
+            return res.status(404).send({ message: 'Student not found' });
         }
 
-        // Find the class (assuming you have a way to determine the class)
-        const classId = '678fc73771e8cb87c6f0dd48'; // Replace with actual class ID
+        //const classId = '678fc73771e8cb87c6f0dd48'; // Replace with actual class ID
+
+        // TODO: trenutno je random al myb bi tribalo da se u body uz uid posalje i koji je kolegij
+        const classIdArray = await Class.aggregate([{ $sample: { size: 1 } }]);
+        const classId = classIdArray[0];
 
         // Create a new attendance record
         const attendance = new Attendance({
-        studentId: student._id,
-        classId: classId,
-        status: 'Prisutan',
-        otherId: uid,
+            studentId: student._id,
+            classId: classId,
+            status: 'Prisutan',
+            studentUID: uid,
         });
 
         await attendance.save();
